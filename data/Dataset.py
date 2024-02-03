161612dfb7fc4e7b2f9a17d4840070cf2550d97d
maaliii <mizrakmehmetali@gmail.com>
2024-02-03 17:00:55 +0300

    commit

--- data/Dataset.py
+++ data/Dataset.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
+import shutil
 
 df_heart = pd.read_csv("heart.csv")
 df_heart["target"] = df_heart["output"]
@@ -10,8 +12,19 @@ df_heart.drop(["output"],inplace=True ,axis=1)
 
 df_heart.drop_duplicates(keep="first", inplace=True)
 
-df_heart.to_csv("data/train_data.csv", index=False)
-df_heart.to_csv("model/train_data.csv", index=False)
-df_heart.to_csv("reports/train_data.csv", index=False)
+csv_filename = "train_data.csv"
+df_heart.to_csv(csv_filename, index=False)
 
+model_dir = "../models"
+data_dir = "data"
+
+if not os.path.exists(model_dir):
+    os.makedirs(model_dir)
+
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+
+shutil.copy(csv_filename, os.path.join(model_dir, csv_filename))
+
+print("Train dataseti CSV olarak kaydedildi ve model/data klasörlerine kopyalandı.")
 
